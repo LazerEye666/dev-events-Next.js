722ffb2599868a3d1eb396f72b895283d676556b
Yujin360 <jek.eliorig@gmail.com>
2025-11-01 22:07:00 -0700

    pushing update for events, slug, components, and database

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
-interface Props{
+
+interface Props {
     title: string;
     image: string;
     slug: string;
@@ -9,33 +9,31 @@ interface Props{
     date: string;
     time: string;
 }
+
 const EventCard = ({ title, image, slug, location, date, time }: Props) => {
-  return (
-    <Link href={'/events/${slug}'} id="event-card">
-        <Image src={image} alt={'title'} width={410} height={300} className="poster"/>
+    return (
+        <Link href={`/events/${slug}`} id="event-card">
+            <Image src={image} alt={title} width={410} height={300} className="poster" />
 
-        <div className="flex flex-row gap-2">
-            <Image src="/icons/pin.svg" alt="location" width={14} height={14}/>
-            <p>{location}</p>
-        </div>
-
-        <p className='title'>{title}</p>
-
-        <div className="datetime">
-            <div>
-                <Image src="/icons/calendar.svg" alt="date" width={14} height={14}/>
-                <p>{date}</p>
+            <div className="flex flex-row gap-2">
+                <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+                <p>{location}</p>
             </div>
-        </div>
-        <div className="datetime">
-            <div>
-                <Image src="/icons/clock.svg" alt="time" width={14} height={14}/>
-                <p>{date}</p>
-            </div>
-        </div>
 
-    </Link>
-  )
+            <p className="title">{title}</p>
+
+            <div className="datetime">
+                <div>
+                    <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+                    <p>{date}</p>
+                </div>
+                <div>
+                    <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+                    <p>{time}</p>
+                </div>
+            </div>
+        </Link>
+    )
 }
 
 export default EventCard
--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import React from 'react'
 import Image from "next/image";
-function ExploreBtn() {
-  return (
-    <button className="mt-5" type="button" id="explore-btn" onClick={() => console.log('CLICK')}>
-        <a href="#events">
-            ExploreBtn
-            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}></Image>
-        </a>
-    </button>
-  )
+
+const ExploreBtn = () => {
+    return (
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+            </a>
+        </button>
+    )
 }
 
 export default ExploreBtn
